ca8e41ee9787380779824abb36ff0e9e713a0e77
Takodepoio <163039765+Takodepoio@users.noreply.github.com>
2024-09-11 13:05:17 -0600

    Hoy vimos javascript y se parece a C un poco

---
+++ 01-fundamentos de programación/comentarios.js
@@ -0,0 +1,21 @@
+// Comentarios en linea
+/* 
+Comentaros en bloque
+
+Para más de una linea de comentarios.
+Se hace con shift + alt + a
+*/
+
+
+// La función alert lanza una ventana de alert
+
+alert("alerta");
+alert("Variables esta enlazado enlazado");
+
+/* 
+Recomendaciones
+
+1. No debemos recargar mucho nuestro código de comentarios.
+2. Los comentarios deben ser minimos, concretos y descriptivos.
+3. No deben ser redundantes respecto al código
+*/
---
+++ 01-fundamentos de programación/variables.js
@@ -0,0 +1,37 @@
+/* 
+Las variables no son más que contenedores almacenadoras de datos.
+
+Con las sigueintes palabras se delcaran variables.
+
+var -----> tradicional sin embargo ya no se recomienda
+let -----> Es la forma moderna y se recomienda
+const----> Es una dorma moderna pero el dato almmacenado no cambia 
+
+Para declarar una variable:
+
+var [nombrevariable] = [valor];
+*/
+var nombre = "Antonio Uribe";
+let edad = 20;
+const vivo = true;
+
+console.log(nombre)
+console.log(edad)
+console.log(vivo)
+
+/* 
+Las variables declaradas con "var" pueden cambiar mas no es necesario declararlas de nuevo
+
+Las variables declaradas con "const" no van a cambiar
+
+Para cambiar el valor de una variable es 
+
+[nombreVariable] = [nuevoValor]
+*/
+
+edad = 24;
+
+
+console.log(edad);
+
+alert(nombre);
