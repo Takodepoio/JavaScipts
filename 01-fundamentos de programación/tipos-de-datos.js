e1e39ae32b76a106c68acdc2f150ba0bc29aac2d
Takodepoio <163039765+Takodepoio@users.noreply.github.com>
2024-09-18 13:05:01 -0600

    aprendimos tipos de datos

---
+++ 01-fundamentos de programación/tipos-de-datos.js
@@ -0,0 +1,57 @@
+// En este código vamos a ver los tipos de datos utilizables en el código
+/* 
+Tipos de datos
+
+los tipos de datos se dividen en primitivos y complejos.
+
+1. Tipos de datos primitivos
+
+Datos basicos que no se consideran "objetos" y no tienen "métodos"
+
+number o número
+
+representación de valores ya sea enteros o con decimales
+(punto flotante)
+
+Tambien hay valores boolianos que son los de verdadero o falso se utilizan para los condicionantes
+
+ademas
+
+Existen las cadenas de caracteres con el formato String y se usa para representar tecto se usa con comillas dobles o simples mientras que seas consistente con el uso de ese signo ("") o ''
+
+Puedes tener variables definidas cuando son inicializadas o indefinidas cuando no han sido inicializadas
+
+Array o arreglos
+
+son un conjunto de datos que los almacena en una lista ordenada de elementos.
+los datos dentro de los arreglos se escriben entre corchetes. [] y separados por comas , 
+
+*/
+let miNumero = 4;
+let miNumeroDecimal= 4.444;
+
+/* console.log(miNumero);
+console.log(miNumeroDecimal); */
+
+let miTexto = "Antonio";
+
+/* console.log(miTexto); */
+
+let miVerdad = true ;
+let miMentira = false;
+
+/* console.log(miVerdad);
+console.log(miMentira);
+ */
+
+let x;
+
+/* x = prompt("cual es tu numero favorito");
+ */
+/* console.log(x);
+alert(x);
+ */
+
+let arreglito = ['manzana','pera','uva','mole'];
+
+console.log(arreglito [0]);
