09f7ee93639f2ab76b489c7b4313805bbb90a38f
Takodepoio <163039765+Takodepoio@users.noreply.github.com>
2024-09-23 13:50:31 -0600

    ya pusimos los operadores

---
+++ 01-fundamentos de programación/operadores.js
@@ -0,0 +1,60 @@
+/* 
+Operadores
+
+Son smbolos utilizados para realizar operaciones sobre valores y variables.
+
+1. Aritmeticos
+2. Asignación
+3. Comparación
+
+*/
+
+/* 
+operadores aritméticos
+
+Se utilizan para realizar cálculos matemáticos.
+
+sumar +
+restar -
+multiplicar * 
+dividir /
+
+*/
+let x = prompt("dame un valor");
+let y = prompt("dame un valor");
+
+let sumar = x + y ;
+alert(sumar);
+
+/* resta */
+let resta = x - y ;
+alert(resta);
+
+/* multiplicación */
+let multiplicacion = x / y ;
+alert(multiplicacion);
+
+//división
+
+let divicion = x / y ;
+alert(divicion);
+
+// módulo (%): Devuelve el resto de la división de los valores.
+
+let mod = 7 % 2; // 1
+console.log(mod);
+
+//Exponenciacion (**) : Eleva un numero a la potencia del otro.
+let expo = 5 ** 3; //
+console.log(expo); 
+
+// Incremento (++): Incremento el valor en uno
+let z = 2;
+z++;
+console.log(z);
+
+// decremento (--): decrese el valor en uno
+let t = 2;
+t--;
+console.log(t);
+
