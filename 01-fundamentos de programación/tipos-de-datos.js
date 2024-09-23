2405cd3573d300bbc193faaebc77ec4055b78a34
Takodepoio <163039765+Takodepoio@users.noreply.github.com>
2024-09-23 13:01:50 -0600

    hoy vimos tipos de datos y ya lo terminamos

--- 01-fundamentos de programación/tipos-de-datos.js
+++ 01-fundamentos de programación/tipos-de-datos.js
@@ -26,6 +26,15 @@ Array o arreglos
 son un conjunto de datos que los almacena en una lista ordenada de elementos.
 los datos dentro de los arreglos se escriben entre corchetes. [] y separados por comas , 
 
+Este tipo de dato es un dato complejo o de objeto.
+
+*object
+Es un conjunto de multiples datos en pares 'clave'-'valor'. Se escriben entre llaves {} y separados por comas.
+
+*funciones o functions
+
+Es un tipo de dato que puede ser 'invocado'. Se declaran con la palabra reservada "function". y el código que ejecutan entre llave {"código"}
+
 */
 let miNumero = 4;
 let miNumeroDecimal= 4.444;
@@ -55,3 +64,30 @@ alert(x);
 let arreglito = ['manzana','pera','uva','mole'];
 
 console.log(arreglito [0]);
+
+/* object objeto */
+
+let persona = {
+
+  /* clave : valor */
+  nombre : "Antonio",
+  edad: 20,
+  colorDePiel: "medio cafe media taza de leche con toque de rosa",
+  altura: 1.88,
+  peso: undefined,
+  vivo: true,
+  novia : false,
+}
+
+console.log(persona);
+//accedemos a los datos de una clabe dentro de un objeto
+//con la anotación de punto .
+
+/* function */
+
+let mensajito = function () {
+  // pasamos el código que ejecuta la funcion
+  console.log("Este es un mensaje lanzado por una funcion")
+}
+//invocamos la función
+mensajito();
