e55c757da624211eb9e33228359c7ee77d404e4f
Takodepoio <163039765+Takodepoio@users.noreply.github.com>
2024-10-14 13:33:23 -0600

    aprendimos switch

---
+++ 01-fundamentos de programación/switch.js
@@ -0,0 +1,116 @@
+/* 
+El switch es una forma de hacer condicionales en distintos casos mas sin enbargo no es putil para ejectar codigos complejos si el condicional se cumple.
+
+Estructura del Switch
+
+switch (condicional){
+
+case : valor1
+
+orden a ejecutar;
+
+breake; // este es importante porque indica que se acaba el caso en ese espacio
+
+case : valor2
+
+orden a ejecutar
+
+break
+
+default : 
+
+orden a ejecutar si no se cumple ningun caso
+}
+*/
+
+/* let day = prompt('que dia es hoy?');
+ 
+switch(day){
+
+    case 'Lunes':
+        alert('hoy es lunes');
+
+        break;
+
+    case 'lunes':
+        alert('hoy es lunes');
+
+        break;
+
+    case 'Martes':
+        alert('Hoy es martes');
+
+        break;
+
+    case 'Martes':
+        alert('Hoy es martes');
+
+        break;
+
+    case 'martes':
+        alert('Hoy es martes');
+
+        break;
+
+    case 'Miercoles':
+        alert ('hoy es miercoles');
+
+        break;
+
+    case 'miercoles':
+        alert('Hoy es miercoles');
+
+        break;
+
+    case 'Jueves':
+        alert('Feliz Jueves de RASISMO');
+
+        break;
+
+    case 'jueves':
+        alert('Feliz Jueves de RASISMO');
+
+        break;
+
+    case 'Viernes':
+        alert('Hoy es viernes tomorrow no hay kinder');
+
+        break;
+
+    case 'viernes':
+        alert('Hoy es viernes tomorrow no hay kinder');
+
+        break;
+
+    case 'Sabado':
+        alert('Hoy es sabado TutanKamon');
+
+        break;
+
+    case 'sabado':
+        alert('Hoy es sabado TutanKamon');
+
+        break;
+
+    case 'Domingo':
+        alert('Tomorrow se vuelve al kinder');
+
+        break;
+
+    case 'domingo':
+        alert('Tomorrow se vuelve al kinder');
+
+        break;
+
+
+        default:
+            alert('escribiste algo mal, chiquitou');
+
+} */
+
+        
+// ejercicio de switch
+// Hacer un switch con una seleccion de 3 pokemones y decir que tipo de pokemon es.
+
+
+
